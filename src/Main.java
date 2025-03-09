e3f7ee3d46379e7fc46bba68a33e45a3035aebaf
Isaac Lee <n12135682@qut.edu.au>
2025-03-09 18:28:33 +1000

    implemented authentications

--- src/Main.java
+++ src/Main.java
@@ -92,7 +92,14 @@ public class Main {
         System.out.print("Enter your password: ");
         String password = scanner.nextLine();
         User user = authService.signUp(username, password);
-        // TODO Later: Shows a message based on the result
+        // TODO Now: Show a message based on the result of the signUp method:
+        // - If the user is not null, show "User <username> has been created successfully!"
+        // - If the user is null, show "The username is already taken!"
+        if(user != null) {
+            System.out.println("User " + user.getUsername() + " has been created successfully!");
+        } else {
+            System.out.println("The username is already taken!");
+        }
     }
 
     /**
